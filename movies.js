2b90bcfb28bebb32055a721331b9b5f75c22708a
Eden Smith <edenwint@gmail.com>
2020-11-29 23:31:54 +0000

    managed to get node working

--- movies.js
+++ movies.js
@@ -1,56 +1,57 @@
-// imports external libraries
+// // imports external libraries
 
-const express = require("express");
-const cors = require ('cors');
-// const { MongoClient, ObjectId } = require("mongodb");
-
-
-const MongoClient = require("mongodb").MongoClient;
-
-// starts express app 
-
-const app = express();
+// const express = require("express");
+// const cors = require ('cors');
+// // const { MongoClient, ObjectId } = require("mongodb");
 
 
-// interact with json data
-app.use(express.json());
+// const MongoClient = require("mongodb").MongoClient;
+// A
+// // starts express app 
 
-
-const client = new MongoClient(process.env.ATLAS_URI, {useUnifiedTopology: true});
-
-
-// db path 
-
-const uri = 'mongodb://localhost:27017'; 
-
-
-// try to connect to MOngodb
-
-mongo.then(() => {
-    console.log('connected');
-}, error => {
-    console.error(error, 'error')
-});
-
-// Check DB Connection 
-
-if (!db)
-    console.log("Error have not connected to Database");
-else
- console.log("You are now connected to the Database");
+// const app = express();
 
 
 
-
-// Server port  
-
-const port = process.env.PORT || 5000;
+// // interact with json data
+// app.use(express.json());
 
 
-// Launch app to specific port 
+// const client = new MongoClient(process.env.ATLAS_URI, {useUnifiedTopology: true});
 
-app.listen(port, function() {
-    console.log('it is working!' + port)
-});
+
+// // db path 
+
+// const uri = 'mongodb://localhost:27017'; 
+
+
+// // try to connect to MOngodb
+
+// // mongo.then(() => {
+// //     console.log('connected');
+// // }, error => {
+// //     console.error(error, 'error')
+// // });
+
+// // Check DB Connection 
+
+// if (!db)
+//     console.log("Error have not connected to Database");
+// else
+//  console.log("You are now connected to the Database");
+
+
+
+
+// // Server port  
+
+// const port = process.env.PORT || 5000;
+
+
+// // Launch app to specific port 
+
+// app.listen(port, function() {
+//     console.log('it is working!' + port)
+// });
 
 
